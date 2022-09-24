44941d93cbaccbf27dee2980dae51af2c31215d4
Jader <62562208+jaderloki@users.noreply.github.com>
2022-09-24 15:32:16 -0300

    Update main.js

--- main.js
+++ main.js
@@ -12,23 +12,31 @@ console.log("carregando");
         elements = document.querySelectorAll("[jader]");
 
         for (var i = 0; i < elements.length; i++) {
-            scramble(elements, i);
+        //for (var i = 0; i < 2; i++) {
+            scramble(i);
         }
     }, 2500);
 };
 
-function scramble(elements, i){
+var aleatorio = null;
+function scramble(i){
     setTimeout(function(){
-        var afs = elements[i].closest("button");
+        var elemento = document.querySelectorAll("[jader='"+i+"']")[0];
+        var afs = elemento.closest("button");
         afs.click();
+        setTimeout(function(){
+            //var aleatorio = Math.floor(Math.random() * (4 - 3 + 1) + 3);
+            aleatorio = (aleatorio != null ? (aleatorio == 4 ? 3 : 4) : Math.floor(Math.random() * (4 - 3 + 1) + 3));
+            //alert(aleatorio);
+            if(aleatorio > 2){
+                elemento.closest(".bg-back-lightest").getElementsByClassName("shadow-popover")[0].getElementsByTagName("button")[aleatorio].click();
+            }else{
 
-        var aleatorio = Math.floor(Math.random() * (4 - 2 + 1) + 2);
-        //alert(aleatorio);
-        if(aleatorio > 2){
-            document.getElementsByClassName("shadow-popover")[0].getElementsByTagName("button")[aleatorio].click();
-        }else{
-
-        }
-        console.log("Iteracao", i, "escolha do numero do botao", aleatorio, "foi");
-    }, 1500 * i);
+            }
+            document.getElementsByClassName("min-h-screen")[0].click();
+            document.getElementsByTagName("body")[0].click();
+            var nomeDaMusica = elemento.closest(".bg-back-lightest").getElementsByClassName("break-words")[0].textContent;
+            console.log("nome da musica", nomeDaMusica, "Iteracao", i, "escolha do numero do botao", aleatorio, "foi");
+        }, 500);
+    }, 1000 * i);
 }
